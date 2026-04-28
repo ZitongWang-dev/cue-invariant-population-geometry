0545e97601212667c161e468e1ead44eaf7d7f2c
Zitong Wang <79509785+ZitongWang-dev@users.noreply.github.com>
2026-04-28 11:17:02 -0400

    Compute affine PT decoding for FRV2

--- decoding-analysis/neuronal_decoding/code/noise_correlation_scripts/procrustes_decoding_trial_perturbed.m
+++ decoding-analysis/neuronal_decoding/code/noise_correlation_scripts/procrustes_decoding_trial_perturbed.m
@@ -42,7 +42,7 @@ clc; clear;
 
 %% Configuration
 monkey = 'FR';      % 'FR' or 'KO'
-vp     = 'V1';      % 'V1' or 'V2'
+vp     = 'V2';      % 'V1' or 'V2'
 
 %% Trial perturbation configuration
 perturb_mode = 'affine';   % 'none' | 'shuffle' | 'affine'
