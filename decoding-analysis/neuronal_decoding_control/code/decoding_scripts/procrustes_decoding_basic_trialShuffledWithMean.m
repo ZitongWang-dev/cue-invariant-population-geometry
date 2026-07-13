95dd6a510509bae89cbe5e797246b9cc7bd02406
Zitong Wang <79509785+ZitongWang-dev@users.noreply.github.com>
2026-07-13 17:13:11 -0400

    Compute FR V1 trialShuffledWithMean

--- decoding-analysis/neuronal_decoding_control/code/decoding_scripts/procrustes_decoding_basic_trialShuffledWithMean.m
+++ decoding-analysis/neuronal_decoding_control/code/decoding_scripts/procrustes_decoding_basic_trialShuffledWithMean.m
@@ -55,7 +55,7 @@ Outputs:
 clc; clear;
 
 %% Configuration
-monkey = 'KO';      % Monkey ID ('FR' or 'KO')
+monkey = 'FR';      % Monkey ID ('FR' or 'KO')
 vp = 'V1';          % Visual area ('V1' or 'V2')
 
 % Load neuronal data for all stimuli
